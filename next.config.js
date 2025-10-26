1d6b4387cbe2ee5656d00a00e17cf4070fa40732
shahisrail <shahisrail134@gmail.com>
2025-10-26 14:40:33 +0600

    chore: remove deprecated swcMinify for Next 16

--- next.config.js
+++ next.config.js
@@ -1,22 +1,21 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
 module.exports = {
   output: "export",
   basePath: "",
   reactStrictMode: false,
   trailingSlash: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
+
+  images: { unoptimized: true },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ["@svgr/webpack"],
     });
-
     return config;
   },
-  compiler: {
-    styledComponents: true,
-  },
+
+  compiler: { styledComponents: true },
 };
